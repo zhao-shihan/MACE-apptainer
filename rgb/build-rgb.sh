a15e8b9a0ea3853b3b084a6f198171b5486b9975
zhaoshihan <zhao_shihan@foxmail.com>
2023-12-07 02:32:22 +0800

    Update

--- rgb/build-rgb.sh
+++ rgb/build-rgb.sh
@@ -1,6 +1,7 @@
 # simd_list=(sse4.2 avx avx2 fma avx512f avx512vl)
 simd_list=(sse4.2 avx avx2 fma)
 
-for simd in simd_list; do
-    apptainer build --build-arg SIMD=${simd} rgb-${simd}.sif rgb.def
+apptainer build rgb-base.sif rgb-base.def
+for simd in $simd_list; do
+    apptainer build --build-arg SIMD=$simd rgb-$simd.sif rgb.def
 done
