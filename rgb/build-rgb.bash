e132f98a9c546fb1aca10e520fbd91bfa578649e
zhaoshihan <zhao_shihan@foxmail.com>
2023-12-08 04:20:49 +0800

    Update

--- rgb/build-rgb.bash
+++ rgb/build-rgb.bash
@@ -18,4 +18,6 @@ apptainer build rgb-tianhe2.sif def/rgb-tianhe2.def
 for ((i = 0; i < ${#tag[@]}; ++i)); do
     apptainer build --build-arg FLAG0=${flag0[$i]} --build-arg FLAG1=${flag1[$i]} --build-arg FLAG2=${flag2[$i]} rgb-${tag[$i]}-mt.sif def/rgb-mt.def
     apptainer build --build-arg FLAG0=${flag0[$i]} --build-arg FLAG1=${flag1[$i]} --build-arg FLAG2=${flag2[$i]} rgb-${tag[$i]}.sif def/rgb.def
+    apptainer build --build-arg LOCALIMAGE=rgb-${tag[$i]}-mt.sif rgb-${tag[$i]}-mt-slim.sif def/rgb-slim.def
+    apptainer build --build-arg LOCALIMAGE=rgb-${tag[$i]}.sif rgb-${tag[$i]}-slim.sif def/rgb-slim.def
 done
